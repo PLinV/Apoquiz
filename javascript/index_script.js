2f24f99acb1745c0341ecce0b148d3fae6334ec5
PLinV <paulinverriere4@gmail.com>
2024-03-23 01:01:46 +0100

    creation

--- javascript/index_script.js
+++ javascript/index_script.js
@@ -1,6 +1,8 @@
 const nbJoueur = document.getElementById("nombrejoueur");
 const listejoueur = document.getElementById("listejoueur");
 let start = document.getElementById("start");
+let nbJoueurlast = 0
+let condition2player = false
 start.disabled = true;
 
 nbJoueur.addEventListener("input", updateValue);
@@ -9,20 +11,44 @@ nbJoueur.addEventListener("input", updateValue);
 //listejoueur.textContent
 
 function updateValue(e) {
-    listejoueur.innerHTML = "";
+    const playerElementSpan = document.createElement("label");
+    const playerElementInput = document.createElement("input");
     start.disabled=false;
-    for (let i = 1; i <= e.target.value; i++){
-        const playerElementSpan = document.createElement("label");
-        const playerElementInput = document.createElement("input");
-        playerElementSpan.textContent = "joueur " + i + " :";
-        playerElementInput.id = "joueurInput" + i;
-        playerElementSpan.id = "joueurSpan" + i;
+    let nbJoueurnow = parseInt(e.target.value)
+    if (nbJoueurnow === 2 & condition2player === false) {
+        const playerElementSpan1 = document.createElement("label");
+        const playerElementInput1 = document.createElement("input");
+        playerElementSpan1.textContent = "joueur " + 1 + " :";
+        playerElementInput1.id = "joueurInput1";
+        playerElementSpan1.id = "joueurSpan1";
+        listejoueur.appendChild(playerElementSpan1);
+        listejoueur.appendChild(playerElementInput1);
+        document.getElementById("joueurSpan1").style.gridRow = 1;
+        document.getElementById("joueurInput1").style.gridRow = 1;
+    
+
+        playerElementSpan.textContent = "joueur " + 2 + " :";
+        playerElementInput.id = "joueurInput" + 2;
+        playerElementSpan.id = "joueurSpan" + 2;
         listejoueur.appendChild(playerElementSpan);
         listejoueur.appendChild(playerElementInput); 
-        document.getElementById("joueurSpan" + i).style.color = "red";
-        document.getElementById("joueurSpan" + i).style.gridRow = i;
-        document.getElementById("joueurInput" + i).style.gridRow = i;
-        document.getElementById("joueurInput" + i).style.position = "relative";
-        document.getElementById("joueurInput" + i).style.top = "0px";
+        document.getElementById("joueurSpan" + 2).style.gridRow = 2;
+        document.getElementById("joueurInput" + 2).style.gridRow = 2;
+
+        condition2player = true
     }
+    else if (nbJoueurlast < nbJoueurnow){
+        playerElementSpan.textContent = "joueur " + nbJoueurnow + " :";
+        playerElementInput.id = "joueurInput" + nbJoueurnow;
+        playerElementSpan.id = "joueurSpan" + nbJoueurnow;
+        listejoueur.appendChild(playerElementSpan);
+        listejoueur.appendChild(playerElementInput);
+        document.getElementById("joueurSpan" + nbJoueurnow).style.gridRow = nbJoueurnow;
+        document.getElementById("joueurInput" + nbJoueurnow).style.gridRow = nbJoueurnow; 
+    }
+    else if (nbJoueurlast > nbJoueurnow){
+        document.getElementById("joueurSpan"+nbJoueurlast).remove();
+        document.getElementById("joueurInput"+nbJoueurlast).remove();
+    }
+    nbJoueurlast = nbJoueurnow
 }
